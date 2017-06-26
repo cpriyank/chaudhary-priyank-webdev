9398f355719fe698ec697fbd89421afdd6697b40
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-06-25 19:25:38 -0700

    forgot to add server.js to index

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var app = express();
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public/assignment'));
+app.use(express.static(__dirname + '/public'));
 
 // require ("./test/app.js")(app);
+require("./assignment/app.js")(app);
 
-var port = process.env.PORT || 3000;
+var port      = process.env.PORT || 3000;
+var ipaddress = process.env.IP;
 console.log('Hello! Server running on localhost port 3000')
 
-app.listen(port);
+app.listen(port, ipaddress);
