4d8e1b90943d37e55fd675746136b3af90638abe
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-07-21 16:43:48 -0700

    config.js: logins, session resolvers, etc

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -23,22 +23,26 @@
 			.when("/user/:uid", {
 				templateUrl: "views/user/profile.view.client.html",
 				controller: "ProfileController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website", {
 				templateUrl: "views/website/website-list.view.client.html",
 				controller: "WebsiteListController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website/new", {
 				templateUrl: "views/website/website-new.view.client.html",
 				controller: "NewWebsiteController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website/:wid", {
 				templateUrl: "views/website/website-edit.view.client.html",
 				controller: "EditWebsiteController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website/:wid/page", {
 				templateUrl: "views/page/page-list.view.client.html",
@@ -48,7 +52,8 @@
 			.when("/user/:uid/website/:wid/page/new", {
 				templateUrl: "views/page/page-new.view.client.html",
 				controller: "NewPageController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website/:wid/page/:pid", {
 				templateUrl: "views/page/page-edit.view.client.html",
@@ -58,12 +63,14 @@
 			.when("/user/:uid/website/:wid/page/:pid/widget", {
 				templateUrl: "views/widget/widget-list.view.client.html",
 				controller: "WidgetListController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when("/user/:uid/website/:wid/page/:pid/widget/new", {
 				templateUrl: "views/widget/widget-chooser.view.client.html",
 				controller: "NewWidgetController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.when('/user/:uid/website/:wid/page/:pid/widget/search', {
 				templateUrl : "views/widget/widget-flickr-search.view.client.html",
@@ -73,15 +80,28 @@
 			.when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
 				templateUrl: "views/widget/widget-edit.view.client.html",
 				controller: "EditWidgetController",
-				controllerAs: "model"
+                controllerAs: "model",
+                resolve: {loggedin: checkLoggedin}
 			})
 			.otherwise({
 				redirectTo: "/"
 			});
-		// trust youtube urls
-		$sceDelegateProvider.resourceUrlWhitelist([
-			'self',
-			'*://www.youtube.com/**'
-		]);
 	}
-})();
+
+    var checkLoggedin = function ($q, $timeout, $http, $location, $rootScope) {
+        var deferred = $q.defer();
+        $http.get('/api/loggedin')
+            .success(function (user) {
+                $rootScope.errorMessage = null;
+                if (user !== '0') {
+                    $rootScope.currentUser = user;
+                    deferred.resolve();
+                } else {
+                    deferred.reject();
+                    $location.url('/');
+                }
+            });
+        return deferred.promise;
+    };
+})
+();
