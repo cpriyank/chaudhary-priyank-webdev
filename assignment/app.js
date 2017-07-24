024b9513e92082b2eef4c1c6d21ab86f59a352d9
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-07-24 14:47:48 -0700

    all auth wordking

--- assignment/app.js
+++ assignment/app.js
@@ -1,24 +1,25 @@
 module.exports = function(app) {
 
-    var connectionString =  null;
+	var connectionString =  null;
 
-    if (process.env.NODE_ENV) {
-        connectionString = 'mongodb://heroku_4l46z2vg:s27f4o6t21svjqf1j6bf38ast9@ds149221.mlab.com:49221/heroku_4l46z2vg';
-    }
-        else
-    {
-        connectionString = 'mongodb://localhost/web_dev'
-    }
+	if (process.env.NODE_ENV) {
+		var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+		var password = process.env.MLAB_PASSWORD_WEBDEV;
+		connectionString = 'mongodb://' + username + ':' + password;
+		connectionString += '@ds149221.mlab.com:49221/heroku_4l46z2vg';
+	}
+	else
+	{
+		connectionString = 'mongodb://localhost/web_dev'
+	}
 
-    var mongoose = require('mongoose');
-    mongoose.connect(connectionString);
+	var mongoose = require('mongoose');
+	mongoose.connect(connectionString);
 
-    mongoose.Promise = require('q').Promise;
+	mongoose.Promise = require('q').Promise;
 
-    require("./services/user.service.server.js")(app);
-    require("./services/website.service.server.js")(app);
-    require("./services/page.service.server.js")(app);
-    require("./services/widget.service.server.js")(app);
+	require("./services/user.service.server.js")(app);
+	require("./services/website.service.server.js")(app);
+	require("./services/page.service.server.js")(app);
+	require("./services/widget.service.server.js")(app);
 };
-
-
